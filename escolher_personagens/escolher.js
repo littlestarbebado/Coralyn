52cee4ede14568a3c9cb4f3d9069007cae004660
monicarod12 <santosmonicarodrigues@gmail.com>
2026-01-14 22:39:51 +0000

    mi

--- escolher_personagens/escolher.js
+++ escolher_personagens/escolher.js
@@ -7,3 +7,45 @@ function escolher(personagem) {
 function abrirConfig() {
     window.location.href = "../config/config.html";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const btnSettings = document.getElementById("btnSettings");
+  const modal = document.getElementById("modalConfig");
+  const closeModal = document.getElementById("closeModal");
+
+  const musica = document.getElementById("musica");
+  const valorMusica = document.getElementById("valorMusica");
+
+  const btnSair = document.getElementById("btnSair");
+
+  // abrir definições
+  btnSettings.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+  });
+
+  // fechar definições
+  closeModal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+  });
+
+  // fechar clicando fora
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.classList.add("hidden");
+    }
+  });
+
+  // slider música (guardar valor)
+  musica.addEventListener("input", () => {
+    valorMusica.textContent = musica.value + "%";
+    localStorage.setItem("volumeMusica", musica.value);
+  });
+
+  // sair para início
+  btnSair.addEventListener("click", () => {
+    window.location.href = "../inicio/inicio.html";
+  });
+
+});
+
--- inicio/inicio.js
+++ inicio/inicio.js
@@ -2,16 +2,16 @@ let musicaFundo;
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  // 🎵 Música de fundo
+  // Música de fundo
   musicaFundo = new Audio("../assets/audio/song.mp3");
   musicaFundo.loop = true;
-  musicaFundo.volume = 0.3; // 🔊 volume correto
+  musicaFundo.volume = 0.3; // volume correto
 
   musicaFundo.play().catch(() => {
     // autoplay bloqueado (normal)
   });
 
-  // ▶️ ativar música no primeiro clique
+  // ativar música no primeiro clique
   document.addEventListener("click", () => {
     if (musicaFundo.paused) {
       musicaFundo.play();
@@ -60,6 +60,16 @@ document.addEventListener("DOMContentLoaded", () => {
   btnNivel.addEventListener("click", () => {
     window.location.href = "../pag_niveis/niveis.html";
     
+  });
+
+  const btnIniciar = document.getElementById("btnIniciar");
+  btnIniciar.addEventListener("click", () => {
+   window.location.href = "../pag_niveis/niveis.html";
+  });
+
+  const btnSessao = document.getElementById("btnSessao");
+  btnSessao.addEventListener("click", () => {
+   window.location.href = "../escolher_personagens/escolher_personagens.html";
   });
 
   btnSair.addEventListener("click", () => {
